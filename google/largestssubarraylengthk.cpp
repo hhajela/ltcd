20f72b8c17f1e77e41d01c2fbd1d89265e063c6a
Harshit Hajela <harshit.hajela@gmail.com>
2019-11-20 20:10:47 -0500

    largest subarray - better implementation

--- google/largestssubarraylengthk.cpp
+++ google/largestssubarraylengthk.cpp
@@ -10,57 +10,28 @@ class Solution
     
     vector<int> LargestSubarrayLengthK(vector<int> vec1, int k)
     {
-        // array of length n will have n-k subarrays of length k
-        // figure out all subarrays of lenght k
-        
-        //start comparing all elements of each subarrays
-        int offset = 0;
-        int startGreatest = 0;
-        int endGreatest = k-1;
-        bool tie = true;
-        vector<bool> eliminatedStarts(vec1.size(), false);
-        
-        while(tie && offset < k)
-        {
-            tie = false;
-            int curGreatestElement = INT_MIN;
-            for (int j=offset;j<=vec1.size()-k+offset;j++)
-            {
-                //cout << "currently considering " << vec1[j] << " ";
-                if (eliminatedStarts[j-offset])
-                    continue;
-                
-                if (vec1[j]>curGreatestElement)
-                {
-                  //cout << "new greatest for offset " << offset << " ";
-                  startGreatest= j-offset;
-                  endGreatest = startGreatest+k-1;
-                  curGreatestElement = vec1[j];
-                }
-                else if (vec1[j] == curGreatestElement)
-                {
-                  //cout << "its a tie ";
-                  tie = true;
-                }
-                else
-                {
-                  eliminatedStarts[j-offset]= true;
-                  //cout << j-offset << " eliminated" << endl;
-                }
-                //cout << endl;
-            }
-            
-            offset++;
-        }
-        
-        vector<int> vecResult;
-        //cout << startGreatest << " " << endGreatest << endl;
-        for(int i =startGreatest; i<=endGreatest; i++)
-            vecResult.push_back( vec1[i]);
-            
-        return vecResult;
-    }
-    
+	    //iterate through all possible subarrays
+	    int bestStart = 0;
+	    for(int i=1; i<=vec1.size()-k; i++)
+	    {
+		    int offset = 0;
+		    while(offset < k)
+		    {
+			    //cout << "comparing " << i << " and " << bestStart << endl; 
+			    if (vec1[i+offset] == vec1[bestStart+offset])
+			    {
+				    offset++;
+				    continue;
+			    }
+
+			    if (vec1[i+offset] > vec1[bestStart+offset])
+				    bestStart=i;
+
+			    break;
+		    }
+	    }
+	    return vector<int>(vec1.begin()+bestStart, vec1.begin()+bestStart+k);
+    }    
 };
 
 int main() 
